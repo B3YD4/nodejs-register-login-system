af53d18a57504193884c630afe6855d814b346b5
B3YD4 <hilecehennemi6@gmail.com>
2021-03-28 00:57:05 +0300

    nodejs-system

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const app = express();
+const User = require('./models/user')
+
+// Veri Tabanı Bağlantısı...
+mongoose.connect('mongodb://localhost/users', {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connection.on('open', () => {
+    console.log('Veri Tabanı Bağlantısı Sağlandı...')
+});
+mongoose.connection.on('error', (err) => {
+    console.log('Veritabanı Bağlantı Hatası: ', err);
+});
+
+// Görüntü Motoru Yüklüyoruz
+app.set('view engine', 'ejs');
+
+// Server'ı Dinlemeye Aldık
+app.listen(3000, (req, res) => {
+    console.log("Server Ayağa Kaldırıldı...");
+});
+
+// Css dosyaları ve Terminal İçin Gerekli İşlemleri Yaptık
+app.use(express.static('css'));
+app.use(morgan('dev'));
+
+// Yönlendirmeler - Şema
+
+app.get('/add', (req, res) => {
+    const user = new User({
+        kullaniciadi: 'BayDS',
+        eposta: 'hilecehennemi6@gmail.com',
+        sifre: '123456789',
+        sifre_tekrar: '123456789'
+    });
+    user.save((err, data) => {
+        if(err)
+            console.log(err);
+        res.json(data);
+    });
+});
+
+app.get('/',  (req, res) => {
+    res.render('index', {title: 'Giriş Yap'});
+});
+
+app.get('/girisyap', (req, res) => {
+    res.redirect('/')
+});
+
+app.get('/kayitol',  (req, res) => {
+    res.render('kayit', {title: 'Kayıt Ol'});
+});
---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    kullaniciadi: {
+        type: String,
+        require: true
+    },
+
+    eposta: {
+        type: String,
+        require: true
+    },
+
+    sifre:{
+        type: String,
+        require: true
+    },
+
+    sifre_tekrar: {
+        type: String,
+        require: true
+    }
+}, {timestamps: true} );
+
+const User = mongoose.model('User',userSchema);
+module.exports = User;
+
+
