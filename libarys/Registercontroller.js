33669859ac9d70e119e7ae5c482ae83283849293
B3YD4 <41930266+B3YD4@users.noreply.github.com>
2021-03-29 21:49:05 +0300

    Add files via upload

---
+++ libarys/Registercontroller.js
@@ -0,0 +1,26 @@
+function verify(){
+    var pass1 = document.getElementById('pass1').value;
+    var pass2 = document.getElementById('pass2').value;
+    var kadi = document.getElementById('kadi').value;
+    var mail = document.getElementById('mail').value;
+
+    if(pass1 != pass2 || pass2 != pass1){
+        
+        Swal.fire({
+            icon: 'error',
+            title: 'Belirttiğiniz Şifreler Uyuşmuyor!',
+            showConfirmButton: false,
+            timer: 1500
+          });
+
+    }else{
+        Swal.fire({
+            icon: 'success',
+            title: 'Kaydınız Başarı ile Tamamlandı!',
+            showConfirmButton: false,
+            timer: 1500
+          });
+          
+    }
+
+}
