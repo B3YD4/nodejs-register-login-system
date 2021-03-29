40b694d5e702c9a15ec3374b045460cce5e4a014
B3YD4 <41930266+B3YD4@users.noreply.github.com>
2021-03-29 21:49:36 +0300

    Add files via upload

---
+++ routes/authRoutes.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+
+// GET İstekleri
+
+router.get('/', (req, res) => {
+    res.render('index', {title: 'Giriş Yap'});
+});
+
+router.get('/kayitol', (req, res) => {
+    res.render('kayit', {title: 'Kayıt Ol'});
+});
+
+// POST İstekleri
+
+router.post('/', (req, res)  => {});
+router.post('/kayitol', (req, res) => {
+
+    const name = req.body.kullaniciadi;
+    const mail = req.body.eposta;
+    const pass = req.body.sifre;
+    const passtry = req.body.sifre_tekrar;
+
+    const user = new User({
+        kullaniciadi: name,
+        eposta: mail,
+        sifre: pass,
+        sifre_tekrar: passtry
+
+    });
+
+    if(pass != passtry || passtry != pass){
+
+        setTimeout(()=>{
+            res.redirect('/kayitol')
+        }, 1600);
+
+    }else{
+        setTimeout(()=>{
+
+            user.save((err,data) => {
+                if(err)
+                    console.log(err);
+                res.redirect('/')
+            });
+
+        },1800);
+
+    }
+});
+
+
+// Yönlendirmeler
+
+router.get('/girisyap', (req, res) => {
+    res.redirect('/')
+});
+
+router.get('/login',(req, res) => {
+    res.redirect('/')
+});
+
+module.exports = router;
