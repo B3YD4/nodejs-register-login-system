171d9098daeacf0de6b511d4b7016b3fb4bba314
B3YD4 <41930266+B3YD4@users.noreply.github.com>
2021-03-30 09:45:32 +0300

    Update Registercontroller.js

--- libarys/Registercontroller.js
+++ libarys/Registercontroller.js
@@ -10,7 +10,7 @@ function verify(){
           icon: 'error',
           title: 'Belirttiğiniz Şifreler Uyuşmamaktadır!',
           showConfirmButton: false,
-          background: '#000',
+          background: '#101010',
           timer: 1500
         });
 
@@ -19,7 +19,7 @@ function verify(){
             icon: 'success',
             title: 'Kaydınız Başarı ile Tamamlandı!',
             showConfirmButton: false,
-            background: '#000',
+            background: '#101010',
             timer: 1500
           });
           
