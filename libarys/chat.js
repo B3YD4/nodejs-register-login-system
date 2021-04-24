3dff3a433e42d7d4a801f84e5fac5e209aaaeb49
B3YD4 <41930266+B3YD4@users.noreply.github.com>
2021-04-24 04:41:01 +0300

    Update chat.js

--- libarys/chat.js
+++ libarys/chat.js
@@ -1,4 +1,4 @@
-const socket = io.connect('https://yazilimblog.ga');
+const socket = io.connect('http://localhost:3000');
 
 const mesaj = document.getElementById('mesaj');
 const gonderbtn = document.getElementById('gonderbtn');
