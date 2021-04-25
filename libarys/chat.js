5c3b63939e149acba2e56678a2d6e412fb568f68
B3YD4 <41930266+B3YD4@users.noreply.github.com>
2021-04-25 20:02:01 +0300

    Update chat.js

--- libarys/chat.js
+++ libarys/chat.js
@@ -5,6 +5,9 @@ const gonderbtn = document.getElementById('gonderbtn');
 const cikti = document.getElementById('cikti');
 const feedback = document.getElementById('feedback');
 const gonderen = document.getElementById('gonderen');
+const sohbet_ekrani = document.getElementById('sohbet-ekrani');
+
+// Mesaj Değerlerini Gönderiyoruz
 
 gonderbtn.addEventListener('click', () => {
 
@@ -13,25 +16,35 @@ gonderbtn.addEventListener('click', () => {
         gonderen: gonderen.innerHTML
     });
 
+    mesaj.value = ""
+
 });
+
+// Mesajı Ekrana yazdırıyoruz ve Scroll Ayarını Yapıyoruz
 
 socket.on('chat', data => {
     cikti.innerHTML += "<h4><strong>" + data.gonderen + ": </strong>" + data.mesaj + "</h4>"
-    mesaj.value = ""
     feedback.innerHTML = ""
+    sohbet_ekrani.scrollTop = sohbet_ekrani.scrollHeight;
 });
+
+// Herhangi Bir Tuşa Basıldıysa yaziyor komutunu emitliyoruz
 
 mesaj.addEventListener('keypress', () => {
     socket.emit('yaziyor',gonderen.innerHTML);
 });
+
+// yaziyor emitini Yakalayıp feedback Kısmına Değerimizi Yazdırıyoruz
+
+socket.on('yaziyor', data => {
+    feedback.innerHTML = '<h5>' + data + " Yazıyor..." + "</h5>"
+});
+
+// Enter Tuşuna Basıldıysa Eğer Gönder Butonuna click Ettiriyoruz
 
 mesaj.addEventListener('keyup', () => {
     if (event.keyCode == 13){
         event.preventDefault();
         gonderbtn.click();
     }
-});
-
-socket.on('yaziyor', data => {
-    feedback.innerHTML = '<h5>' + data + " Yazıyor..." + "</h5>"
 });
